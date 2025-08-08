c2e68860ac1ef9acdeedda27236b4f9e1d3a72ef
Jacek Pudysz <jacekpudysz@gmail.com>
2025-08-08 07:11:45 +0200

    chore: reorganize screen dimensions under nested screen object in UniwindRuntime

--- packages/uniwind/src/components/runtime.ts
+++ packages/uniwind/src/components/runtime.ts
@@ -4,8 +4,10 @@ import type { UniwindRuntime as UniwindRuntimeType } from './types'
 const window = Dimensions.get('window')
 
 export const UniwindRuntime = {
-    screenWidth: window.width,
-    screenHeight: window.height,
+    screen: {
+        width: window.width,
+        height: window.height,
+    },
     colorScheme: Appearance.getColorScheme() ?? 'light',
     orientation: window.width > window.height ? 'landscape' : 'portrait',
     rem: PixelRatio.getFontScale() * 16,
